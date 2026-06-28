c91e6ab5c11dae2a99bf26928173a8fbffd7d0f8
shenxiangyu <xiaobei1009@163.com>
2026-06-28 17:46:45 +0800

    refactor: 按钮移至地图下方，信息栏在按钮下方

--- client/src/scenes/BattleScene.ts
+++ client/src/scenes/BattleScene.ts
@@ -103,15 +103,15 @@ export class BattleScene extends Phaser.Scene {
       fontSize: '13px', color: '#aaaaaa', fontFamily: 'monospace',
     }).setOrigin(0.5, 1).setDepth(10)
 
-    this.unitInfoText = this.add.text(400, 484, '', {
+    this.btnConfirm = this.createButton(310, 505, 100, 32, '确认移动', 0x44aa44, () => this.onBtnConfirm())
+    this.btnCancel = this.createButton(430, 505, 100, 32, '取消移动', 0x666666, () => this.onBtnCancel())
+    this.btnSkipMove = this.createButton(190, 505, 100, 32, '不移动', 0x4488cc, () => this.onBtnSkipMove())
+    this.btnDeselect = this.createButton(70, 505, 100, 32, '取消选择', 0x666666, () => this.onBtnDeselect())
+
+    this.unitInfoText = this.add.text(400, 555, '', {
       fontSize: '15px', color: '#ffffff', fontFamily: 'monospace',
       lineSpacing: 8, align: 'center',
     }).setOrigin(0.5, 0).setDepth(10)
-
-    this.btnConfirm = this.createButton(310, 620, 100, 32, '确认移动', 0x44aa44, () => this.onBtnConfirm())
-    this.btnCancel = this.createButton(430, 620, 100, 32, '取消移动', 0x666666, () => this.onBtnCancel())
-    this.btnSkipMove = this.createButton(190, 620, 100, 32, '不移动', 0x4488cc, () => this.onBtnSkipMove())
-    this.btnDeselect = this.createButton(70, 620, 100, 32, '取消选择', 0x666666, () => this.onBtnDeselect())
     this.btnConfirm.setVisible(false)
     this.btnCancel.setVisible(false)
     this.btnSkipMove.setVisible(false)
